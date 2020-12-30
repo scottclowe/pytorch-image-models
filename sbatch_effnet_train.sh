d2f8d294617098a0ab5d245d8345416cb5d8a211
rearle <rb504035@dal.ca>
2020-12-30 13:42:12 -0400

    Job: test
    Testing out validation script with pre-trained checkpoint

---
+++ sbatch_effnet_train.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH -p t4v2
+#SBATCH --exclude=gpu102
+#SBATCH --exclude=gpu115
+#SBATCH --gres=gpu:1                        # request GPU(s)
+#SBATCH --qos=normal
+#SBATCH -c 4                                # number of CPU cores
+#SBATCH --mem=8G                            # memory per node
+#SBATCH --time=64:00:00                     # max walltime, hh:mm:ss
+#SBATCH --array=0%1                    # array value
+#SBATCH --output=logs_new/test/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=test
+
+source ~/.bashrc
+source activate ~/venvs/efficientnet_train
+
+SAVE_PATH="$1"
+SEED="$SLURM_ARRAY_TASK_ID"
+
+touch /checkpoint/robearle/${SLURM_JOB_ID}
+CHECK_DIR=/checkpoint/robearle/${SLURM_JOB_ID}
+
+# Debugging outputs
+pwd
+which conda
+python --version
+pip freeze
+
+echo ""
+python -c "import torch; print('torch version = {}'.format(torch.__version__))"
+python -c "import torch.cuda; print('cuda = {}'.format(torch.cuda.is_available()))"
+python -c "import scipy; print('scipy version = {}'.format(scipy.__version__))"
+python -c "import sklearn; print('sklearn version = {}'.format(sklearn.__version__))"
+python -c "import matplotlib; print('matplotlib version = {}'.format(matplotlib.__version__))"
+echo ""
+
+echo "SAVE_PATH=$SAVE_PATH"
+echo "SEED=$SEED"
+
+#python train.py /imagenet/ --model efficientnet_b0 -b 384 --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa rand-m9-mstd0.5 --remode pixel --reprob 0.2 --amp --lr .048
+python validate.py /scratch/ssd001/datasets/imagenet/val/ --model tf_efficientnet_b0 --checkpoint efficientnet-b0.tar.gz
