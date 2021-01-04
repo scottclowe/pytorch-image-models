6766a7322be585ea26740ce7528cd39948d3ca13
rearle <rb504035@dal.ca>
2021-01-04 13:48:13 -0400

    Job: test_ho
    Testing out higher order activation functions for transfer learning

--- sbatch_effnet_train.sh
+++ sbatch_effnet_train.sh
@@ -8,14 +8,14 @@
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=64:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0%1                    # array value
-#SBATCH --output=logs_new/ef_tl_9full/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=ef_tl_9full
+#SBATCH --output=logs_new/test_ho/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=test_ho
 
 source ~/.bashrc
 source activate ~/venvs/efficientnet_train
 
 SAVE_PATH="$1"
-LR="$2"
+ACTFUN="$2"
 SEED="$SLURM_ARRAY_TASK_ID"
 
 touch /checkpoint/robearle/${SLURM_JOB_ID}
@@ -39,4 +39,4 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python train.py /scratch/ssd001/datasets/imagenet/ --seed $SEED --model tf_efficientnet_b0 --pretrained --tl --output $SAVE_PATH --resume $CHECK_PATH --epochs 5 --amp --lr $LR --batch-size 256 --tl-layers 9full
+python train.py /scratch/ssd001/datasets/imagenet/ --seed $SEED --model tf_efficientnet_b0 --pretrained --tl --output $SAVE_PATH --resume $CHECK_PATH --epochs 5 --amp --lr 1e-3 --batch-size 256 --tl-layers 8full_9full --actfun $ACTFUN --p 2 --k 2
