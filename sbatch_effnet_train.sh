42ea0a0f07039e060893949efac490e819c7b016
rearle <rb504035@dal.ca>
2021-01-06 20:10:50 -0400

    Job: ef_ho_amp
    Training full EfficientNet using training script given in docs. Added
    amp. Runninig higher order activations for non-amp functions.

--- sbatch_effnet_train.sh
+++ sbatch_effnet_train.sh
@@ -8,8 +8,8 @@
 #SBATCH --mem=128G                           # memory per node
 #SBATCH --time=500:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0%1                    # array value
-#SBATCH --output=logs_new/ef_ho_amp/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=ef_ho_amp
+#SBATCH --output=logs_new/ef_ho_noamp/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=ef_ho_noamp
 
 source ~/.bashrc
 source activate ~/venvs/efficientnet_train
@@ -39,4 +39,4 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-./distributed_train.sh 4 /scratch/ssd001/datasets/imagenet/ --model efficientnet_b0 -b 384 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa original --remode pixel --reprob 0.2 --lr .096 --amp
+./distributed_train.sh 4 /scratch/ssd001/datasets/imagenet/ --model efficientnet_b0 -b 384 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa original --remode pixel --reprob 0.2 --lr .096
