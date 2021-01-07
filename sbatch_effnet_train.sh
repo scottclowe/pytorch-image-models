78b0d1d6defe71a0d998563ba500654f44af0ab0
rearle <rb504035@dal.ca>
2021-01-06 20:11:06 -0400

    Job: ef_ho_noamp
    Training full EfficientNet using training script given in docs. Added
    amp. Runninig higher order activations for non-amp functions.

--- sbatch_effnet_train.sh
+++ sbatch_effnet_train.sh
@@ -40,3 +40,4 @@ echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
 ./distributed_train.sh 4 /scratch/ssd001/datasets/imagenet/ --model efficientnet_b0 -b 384 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa original --remode pixel --reprob 0.2 --lr .096
+
