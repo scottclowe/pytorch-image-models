28b7dbbb4a20c8c4fa741cc53eda9cd970bc6792
rearle <rb504035@dal.ca>
2021-01-08 18:07:37 -0400

    Job: ef_swish_apex
    Testing if apex breaks EMA

--- sbatch_effnet_train.sh
+++ sbatch_effnet_train.sh
@@ -3,28 +3,24 @@
 #SBATCH --exclude=gpu102
 #SBATCH --exclude=gpu115
 #SBATCH --gres=gpu:4                        # request GPU(s)
-#SBATCH --qos=normal
+#SBATCH --qos=high
 #SBATCH -c 24                                # number of CPU cores
 #SBATCH --mem=128G                           # memory per node
 #SBATCH --time=500:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0%1                    # array value
-#SBATCH --output=logs_new/ef_higher_ord/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=ef_higher_ord
+#SBATCH --output=logs_new/ef_swish_apex/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=ef_swish_apex
 
 source ~/.bashrc
 source activate ~/venvs/efficientnet_train
 
-ACTFUN="$1"
-AMP="$2"
+SAVE_PATH="$1"
+ACTFUN="$2"
+AMP="$3"
 SEED="$SLURM_ARRAY_TASK_ID"
 
-SAVE_PATH=~/pytorch-image-models/outputs/ef_higher_ord
+touch /checkpoint/robearle/${SLURM_JOB_ID}
 CHECK_PATH=/checkpoint/robearle/${SLURM_JOB_ID}
-IMGNET_PATH=/scratch/ssd001/datasets/imagenet/
-
-mkdir ~/pytorch-image-models/logs_new/ef_higher_ord
-mkdir $SAVE_PATH
-touch $CHECK_PATH
 
 # Debugging outputs
 pwd
@@ -44,5 +40,4 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-./distributed_train.sh 4 $IMGNET_PATH --model efficientnet_b0 -b 192 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa original --remode pixel --reprob 0.2 --lr .048 --control-amp $AMP
-
+./distributed_train.sh 4 /scratch/ssd001/datasets/imagenet/ --model efficientnet_b0 -b 192 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa original --remode pixel --reprob 0.2 --lr .048 --control-amp $AMP
