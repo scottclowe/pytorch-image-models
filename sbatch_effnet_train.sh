42e89e8165f2e33577c86671d34c8da1b4a3745e
rearle <rb504035@dal.ca>
2021-01-12 23:46:00 -0400

    Job: nef_full
    Reverting to full to run swish

--- sbatch_effnet_train.sh
+++ sbatch_effnet_train.sh
@@ -8,8 +8,8 @@
 #SBATCH --mem=128G                           # memory per node
 #SBATCH --time=500:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0%1                    # array value
-#SBATCH --output=logs_new/nef_partial/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=nef_partial
+#SBATCH --output=logs_new/nef_full/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=nef_full
 
 source ~/.bashrc
 source activate ~/venvs/efficientnet_train
@@ -17,10 +17,9 @@ source activate ~/venvs/efficientnet_train
 ACTFUN="$1"
 AMP="$2"
 MULT="$3"
-PARTIAL="$4"
 SEED="$SLURM_ARRAY_TASK_ID"
 
-SAVE_PATH=~/pytorch-image-models/outputs/nef_partial
+SAVE_PATH=~/pytorch-image-models/outputs/nef_full
 CHECK_PATH="/checkpoint/$USER/${SLURM_JOB_ID}"
 IMGNET_PATH=/scratch/ssd001/datasets/imagenet/
 
@@ -44,5 +43,4 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-./distributed_train.sh 8 $IMGNET_PATH --model efficientnet_b0 -b 64 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 4 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa original --remode pixel --reprob 0.2 --lr .032 --control-amp $AMP --extra-channel-mult $MULT --weight-init orthogonal --partial_ho_actfun $PARTIAL
-
+./distributed_train.sh 8 $IMGNET_PATH --model efficientnet_b0 -b 64 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 4 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa original --remode pixel --reprob 0.2 --lr .032 --control-amp $AMP --extra-channel-mult $MULT --weight-init orthogonal
