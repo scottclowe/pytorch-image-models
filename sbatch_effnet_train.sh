d59f90d3228464c6e63dd68e76f61bdc8787cdd6
rearle <rb504035@dal.ca>
2021-01-12 22:01:07 -0400

    Job: nef_full
    Running normalized activation functions. Normal QOS.

--- sbatch_effnet_train.sh
+++ sbatch_effnet_train.sh
@@ -3,7 +3,7 @@
 #SBATCH --exclude=gpu102
 #SBATCH --exclude=gpu115
 #SBATCH --gres=gpu:8                        # request GPU(s)
-#SBATCH --qos=high
+#SBATCH --qos=normal
 #SBATCH -c 32                                # number of CPU cores
 #SBATCH --mem=128G                           # memory per node
 #SBATCH --time=500:00:00                     # max walltime, hh:mm:ss
