0a73e9d2faad4d2c6efea20004aa0a3719b0d277
Scott Lowe <scott.code.lowe@gmail.com>
2021-01-14 01:02:12 +0000

    MNT: kill -9 logger

--- sbatch_effnet_train.sh
+++ sbatch_effnet_train.sh
@@ -50,4 +50,5 @@ export LOGGER_PID="$!"
 
 ./distributed_train.sh 8 $IMGNET_PATH --model efficientnet_b0 -b 64 --actfun $ACTFUN --output $SAVE_PATH --check-path $CHECK_PATH --seed $SEED --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 4 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --aa original --remode pixel --reprob 0.2 --lr .032 --control-amp $AMP --extra-channel-mult $MULT --weight-init orthogonal --partial_ho_actfun $PARTIAL
 
-kill $LOGGER_PID
+kill "$LOGGER_PID"
+kill -9 "$LOGGER_PID"
